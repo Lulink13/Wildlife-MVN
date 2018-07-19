ae19effd67b69e23a0042fa45864cdb8d4865dd5
Jaycee-Speedbug <speedbug@hotmail.fr>
2018-07-19 21:36:05 +0100

    Added: New SQL script file, to be used with sqlcmd
    Signed-off-by: Jaycee-Speedbug <speedbug@hotmail.fr>

---
+++ SQL/DB_CREATION_1.sql
@@ -0,0 +1,33 @@
+/*
+
+ Script de création de la base de données WildLife 2018
+ Pour SQL Server 2017 
+ Jaycee, sur la base du travail de Lulink
+ 
+ 
+ Script Version 0.1
+*/
+
+
+CREATE DATABASE wildlife;
+GO
+
+CREATE TABLE Cartes (	IDCarte 	SMALLINT IDENTITY(0,1),
+			NomCarte 	VARCHAR(15),
+			Largeur 	SMALLINT,
+			Auteur 		VARCHAR(15));
+GO
+
+CREATE TABLE Cellules (	IDCarte 	SMALLINT,
+			CoordX 		SMALLINT,
+			CoordY 		SMALLINT,
+			IDBiome 	TINYINT,
+			Humidite 	TINYINT,
+			Vegetation 	TINYINT);
+GO
+
+CREATE TABLE Biomes (	IDBiome 	SMALLINT IDENTITY(0,1),
+			NomBiome, 	VARCHAR(15),
+			HumiditeBase 	TINYINT,
+			VegetationBase 	TINYINT);
+GO
